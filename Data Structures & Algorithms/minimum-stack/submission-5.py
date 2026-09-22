795447b045e9955a56dcba4d11a9bea0e90b6b7d
J <atomicjab@proton.me>
2026-09-21 19:52:17 -0600

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.py
@@ -0,0 +1,26 @@
+import numpy
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        
+
+    def push(self, val: int) -> None:
+
+        curr_min = val if not self.stack else min(val, self.stack[-1][1])
+        self.stack.append((val, curr_min))
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+        
+
+    def top(self) -> int:
+        v, m = self.stack[-1]
+        return v
+        
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
+        
